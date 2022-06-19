48d04d2d5febac3fbc8f705fa24c4b21c47a3aa8
David <dadastudios382@gmail.com>
2022-06-19 14:13:40 -0500

    Updates

---
+++ precache-manifest.084d6329246c1481f511a95897411c68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cce33912e1d162b9dc9",
+    "url": "/react-loan-simulator/static/css/main.5718a297.chunk.css"
+  },
+  {
+    "revision": "8cce33912e1d162b9dc9",
+    "url": "/react-loan-simulator/static/js/main.8cce3391.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/react-loan-simulator/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "6d10e2e2c9d7011eb9af",
+    "url": "/react-loan-simulator/static/css/2.0fed39b1.chunk.css"
+  },
+  {
+    "revision": "6d10e2e2c9d7011eb9af",
+    "url": "/react-loan-simulator/static/js/2.6d10e2e2.chunk.js"
+  },
+  {
+    "revision": "48b08a09fac489bb277d",
+    "url": "/react-loan-simulator/static/js/runtime~main.48b08a09.js"
+  },
+  {
+    "revision": "9deb55daa66a8cc0ec6fe9ad64fd7a19",
+    "url": "/react-loan-simulator/index.html"
+  }
+];
